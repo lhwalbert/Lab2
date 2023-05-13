b329479cf38b3ba1cd4c48956978470af08dbd5a
Albert <lhwalbert.22@ichat.sp.edu.sg>
2023-05-13 12:09:48 +0800

    update

--- bmi.py
+++ bmi.py
@@ -5,8 +5,11 @@ def calculate_bmi(height, weight):
     print("BMI = " + str(bmi))
     if bmi < 18.5:
         print("You are Under Weight")
+        return -1
     elif 18.5 <= bmi <= 25.0:
         print("You are Normal Weight")
+        return 0
     else:
         print("You are Overweight Weight")
+        return 1
         
